0b31b65cbb81d5b2bcd53cb077465f5899dfbe6d
SpzGen <zvezdochkin@mail.ru>
2020-03-01 14:48:08 +0300

    fix: add functions min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
+  if (array == undefined || array.length == 0) {
   return 0;
+} else {
+  let min = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+}
+return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let avg = 0;
+    for (let i = 0; i < array.length; i++) {
+      avg += array[i];
+    }
+  }
+  return avg;
 }
